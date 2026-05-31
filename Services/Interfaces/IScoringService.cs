29fd7bba6639559075b8b3723066883282439c91
abhipedia <35491823+abhipedia@users.noreply.github.com>
2026-05-31 17:53:48 +0530

    Delete Services/Interfaces/IScoringService.cs

--- Services/Interfaces/IScoringService.cs
+++
@@ -1,8 +0,0 @@
-using IngredientIQ.Services.Models;
-
-namespace IngredientIQ.Services.Interfaces;
-
-public interface IScoringService
-{
-    ScoringResult Calculate(List<MatchResult> matches, string? productCategory, List<string>? userAllergens);
-}
