2e44da34e809c1215d59ef10a1e21e0ead433e5b
abhipedia <35491823+abhipedia@users.noreply.github.com>
2026-05-31 17:53:34 +0530

    Delete Services/Interfaces/IIngredientMatchingService.cs

--- Services/Interfaces/IIngredientMatchingService.cs
+++
@@ -1,9 +0,0 @@
-using IngredientIQ.Services.Models;
-
-namespace IngredientIQ.Services.Interfaces;
-
-public interface IIngredientMatchingService
-{
-    Task<MatchResult> MatchAsync(string rawName);
-    Task<List<MatchResult>> MatchAllAsync(List<string> rawNames);
-}
